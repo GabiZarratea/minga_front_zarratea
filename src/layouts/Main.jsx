c97713d22c71a50d9de014e6c3ee67eeacdbd75d
Donato Zollo <davinazarra@gmail.com>
2023-06-28 00:19:42 -0300

    minga-10

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,11 +2,11 @@
 import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
 
-export default function Main({children}){
+export default function Main({ children }){
     return (
         <div className="shrink-0 bg-cover bg-center bg-no-repeat bg-[url('/background-1.png')] md:bg-[url('/background-1-web.png')] flex flex-col min-h-screen w-full justify-between items-center">
             <NavBar />
-            {children}
+            { children }
             <Footer />
         </div>
     )
