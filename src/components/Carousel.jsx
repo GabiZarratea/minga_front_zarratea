b3d07e13b004486eca3692b833d1e12cbc828c76
Donato Zollo <davinazarra@gmail.com>
2023-06-28 11:45:05 -0300

    Carousel active

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
 import Arrow from './Arrow'
 
 export default function Carousel() {
+
+    const[categories, setCategories] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/categories")
+        .then(res => setCategories(res.data.response))
+        .catch(err => console.log(err))
+    }, [])
+
+    let [counter, setCounter] = useState(0)
+    let next = () => (counter !== categories.length-1) ? setCounter(counter+1) : setCounter(0)
+    let prev = () => (counter !== 0) ? setCounter(counter-1) : setCounter(categories.length-1)
 
     let left = "M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
     let right = "M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
@@ -9,14 +22,18 @@ export default function Carousel() {
     return (
     <div className="hidden lg:flex justify-center items-center w-full h-[100%]">
         <div className='flex text-white w-[90%] h-[75%] rounded-md bg-gradient-to-b from-orange-600 to-orange-500 justify-around gap-[15px] items-center p-2'>
-            <Arrow icon = {left} />
-            <img className='h-[120%] max-w-[230px]' src="/image1.png" />
-            <img className='h-[120%] pb-[35px] rounded-md max-w-[170px]' src="/image2.png" />
+            <button className='button' onClick={prev}>
+                <Arrow icon = {left} />
+            </button>
+            <img className='h-[120%] pb-[35px] max-w-[230px]' src={categories[counter]?.character_photo} />
+            <img className='h-[120%] pb-[35px] rounded-md max-w-[170px]' src={categories[counter]?.cover_photo} />
             <div className='flex flex-col w-[40%] gap-4 ms-3'>
-                <p className='font-medium text-[24px]'>Shonen</p>
-                <p className='w-[90%] text-[14px]'>Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
+                <p className='font-medium text-[24px]'>{categories[counter]?.name}</p>
+                <p className='w-[90%] text-[14px]'>{categories[counter]?.description}</p>
             </div>
-            <Arrow icon = {right} />
+            <button className='button' onClick={next}>
+                <Arrow icon = {right}/>
+            </button>
         </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,65 +5,40 @@ export default function NavBar() {
   const [display, setDisplay] = useState(false)
   return (
     <nav className="w-full h-[10vh] lg:h-[60vh] text-white">
-        <div className="lg:hidden flex w-[90%] justify-between items-center m-3.5">
-
-        {
-              display && (<div className='drawer flex w-[100vw] h-[100vh] flex-col items-start gap-[147px] p-6 bg-gradient-to-b from-orange-600 to-orange-500 fixed top-0 left-0 shadow-2xl hover:bg-white'>
-                <div className='flex h-[525px] flex-col items-center justify-left gap-8 self-stretch'>
-                  <div className='flex'>
-                  <img src="/UserImage.png"/>
-                  <div className='flex flex-col ms-3'>
-                  <p className='text-[16px]'>Lucas Ezequiel Silva</p>
-                  <p className='text-[14px]'>lucasezequielsilva@gmail.com</p>
-                  </div>
-                  <img src="/filled.png" onClick={() => setDisplay(!display)} className='ms-[20%] w-[24px] h-[24px]'/>
-                  </div>
-                  <div className='text-center'>
-                  <p className='p-3 bg-white w-[80vw] rounded-md text-orange-600'>Home</p>
-                  <p className='p-3 w-[80vw]'>Comics</p>
-                  <p className='p-3 w-[80vw]'>My Comics</p>
-                  <p className='p-3 w-[80vw]'>Favorites</p> 
-                  <p className='p-3 w-[80vw]'>Logout</p> 
-                  </div>
-
-                </div>
-              </div>)
-            }
-            <img clasName= "w-[70px] h-[70px] shrink-0" src="/Menu.svg" onClick={() => setDisplay(!display)}/>
-            <img src="/logoMovile.png" className="w-[40px] h-[40px] shrink-0"/>
-        </div>
-        <div className="hidden lg:flex flex-col items-center justify-around">
+      <div className="flex flex-col items-center justify-around">
             <div className="flex w-[90%] justify-between p-6">
+            <img src="/Menu.svg" onClick={() => setDisplay(!display)}/>
             {
-              display && (<div className='drawer hover:bg-whiteflex min-w-[375px] h-[100vh] flex-col items-start gap-[147px] p-6 bg-gradient-to-b from-orange-600 to-orange-500 fixed top-0 left-0 shadow-2xl'>
-                <div className='flex h-[525px] flex-col items-start gap-8 self-stretch'>
-                  <div className='flex'>
-                  <img src="/UserImage.png"/>
-                  <div className='flex flex-col ms-3'>
-                  <p className='text-[16px]'>Lucas Ezequiel Silva</p>
-                  <p className='text-[14px]'>lucasezequielsilva@gmail.com</p>
+              display && (<div className='drawer sm:flex text-center sm:text-start min-w-[100%] sm:min-w-[410px] h-[100vh] flex-col sm:items-start gap-[147px] p-6 bg-gradient-to-b from-orange-600 to-orange-500 fixed top-0 left-0 shadow-2xl'>
+                <div className='flex h-[525px] flex-col items-center sm:items-start gap-8 self-stretch'>
+                <div className='flex w-full justify-end'>
+                <img src="/filled.png" onClick={() => setDisplay(!display)} className='sm:hidden flex justify-end ms-[20%] h-[24px]'/>
+                </div>
+                  <div className='flex flex-row items-center text-center lg:justify-between sm:w-[400px] w-[250px]'>
+                    <img src="/UserImage.png" className='w-[50px] mb-2 sm:m-0'/>
+                    <div className='flex flex-col ms-3'>
+                      <p className='text-[16px]'>Lucas Ezequiel Silva</p>
+                      <p className='text-[14px]'>lucasezequielsilva@gmail.com</p>
+                    </div>
+                    <img src="/filled.png" onClick={() => setDisplay(!display)} className='hidden sm:block ms-[20%] w-[24px] h-[24px]'/>
                   </div>
-                  <img src="/filled.png" onClick={() => setDisplay(!display)} className='ms-[20%] w-[24px] h-[24px]'/>
+                  <div className='lg:text-lg'>
+                    <p className='p-3 bg-white text-orange-600 w-[250px] sm:w-[400px] rounded-md'>Home</p>
+                    <p className='p-3'>Comics</p>
+                    <p className='p-3'>My Comics</p>
+                    <p className='p-3'>Favorites</p> 
+                    <p className='p-3'>Logout</p> 
                   </div>
-                  <div>
-                  <p className='p-5'>Home</p>
-                  <p className='p-5'>Comics</p>
-                  <p className='p-5'>My Comics</p>
-                  <p className='p-5'>Favorites</p> 
-                  <p className='p-5'>Logout</p> 
-                  </div>
-
                 </div>
               </div>)
             }
-            <img src="/Menu.svg"  onClick={() => setDisplay(!display)}/>
             <img src="/logoDos.png" className="w-[193px] h-[65px] shrink-0"/>
             </div>
-            <div className ='text-white inline-flex flex-col items-start gap-[15px] justify-center mt-[5%] w-[90%] p-6'>
+            <div className ='hidden text-white lg:inline-flex flex-col items-start gap-[15px] justify-center mt-[5%] w-[90%] p-6'>
                 <p className="flex text-[64px] font-bold leading-[95.187%]">For the love of manga</p>
                 <p className="text-2xl leading-[95.187%]">Explore our varieties</p>
                 <p className="font-semibold leading-[95.187%]">#MingaLove❤️</p>
-                <Button title="Sign In!" href="" />
+                <Button title="Sign In!" href=""/>
             </div>
         </div>
     </nav>
